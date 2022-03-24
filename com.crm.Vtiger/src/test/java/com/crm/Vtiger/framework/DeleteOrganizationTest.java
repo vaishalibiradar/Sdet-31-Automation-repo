68cffcc817aed02f3e61a8e48606893c7f11c5fc
Vaishali <Vaishali@DESKTOP-2F977B9>
2022-03-24 09:41:15 +0530

    second push

---
+++ com.crm.Vtiger/src/test/java/com/crm/Vtiger/framework/DeleteOrganizationTest.java
@@ -0,0 +1,12 @@
+package com.crm.Vtiger.framework;
+
+import org.testng.annotations.Test;
+
+public class DeleteOrganizationTest {
+	@Test
+	public void deleteOrg() 
+	{
+		System.out.println("Organization is deleted");
+		
+	}
+}
